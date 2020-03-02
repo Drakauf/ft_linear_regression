63afbcc209b241c8f502e9effd08603f0afe02fc
Shankithan Thevakumar <shthevak@student.le-101.fr>
2020-03-02 11:18:00 +0100

    helper

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+import sys
+import helper as h
+
+def checkArg(argv):
+    nbArg = len(argv);
+    print(nbArg)
+    if (nbArg) > 1:
+        if (argv[1] == "-e"):
+            return([1, argv[2]])
+        if (argv[1] == "-t" and nbArg == 4):
+            return([2, argv[2], argv[3]])
+    h.help()
+
 if __name__ == "__main__":
+    launch = checkArg(sys.argv);
+    print("launch")
+    print(launch[1])
     #verify Args and route to estimater
 
     #verify Args and route to trainer
 
     #verify Args and route to helper
+    #Sys exit in helper
     print("main");
